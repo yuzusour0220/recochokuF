42a5ad348b6db94959071360dfb2b22d23b3f4a5
taishi0912 <tee.ypu0912@gmail.com>
2024-11-23 14:42:29 +0900

    test

---
+++ music-teacher-app/src/components/match.js
@@ -0,0 +1,13 @@
+// src/components/match.js の基本構造例
+import React from 'react';
+
+const Match = () => {
+    return (
+        <div>
+            <h1>Match Component</h1>
+            {/* ここに必要なコードを追加 */}
+        </div>
+    );
+};
+
+export default Match;
